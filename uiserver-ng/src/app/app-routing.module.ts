8aab5cff5cbc117571a87f8220a96f2535d42454
kwak70@epozen.com <kwak70@epozen.com>
2020-05-15 18:49:38 +0900

    1

--- uiserver-ng/src/app/app-routing.module.ts
+++ uiserver-ng/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'table', pathMatch: 'full' },
+  { path: '', redirectTo: 'yguide/dbtable', pathMatch: 'full' },
   { path: 'yguide', loadChildren: () => import('./yguide/dbtable/dbtable.module').then(m => m.DbtableModule) },
 
 
-  { path: 'table', loadChildren: () => import('./table/table.module').then(m => m.TableModule) },
-  { path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
-  { path: 'page-header', loadChildren: () => import('./page-header/page-header.module').then(m => m.PageHeaderModule) },
+  { path: 'ztable', loadChildren: () => import('./ztable/table.module').then(m => m.TableModule) },
+  { path: 'zform', loadChildren: () => import('./zform/form.module').then(m => m.FormModule) },
+  { path: 'zpage-header', loadChildren: () => import('./zpage-header/page-header.module').then(m => m.PageHeaderModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
